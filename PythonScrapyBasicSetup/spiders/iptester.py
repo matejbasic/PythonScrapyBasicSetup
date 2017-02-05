e06e9564ed7549a407519516c9591150807bd664
matejbasic91@gmail.com <matejbasic91@gmail.com>
2017-02-05 23:24:03 +0100

    test spiders updated

--- PythonScrapyBasicSetup/spiders/iptester.py
+++ PythonScrapyBasicSetup/spiders/iptester.py
@@ -9,12 +9,18 @@ class IPTesterSpider(scrapy.Spider):
     name = 'IPtester'
     allowed_domains = ['whatismyipaddress.com']
     start_urls = (
-        'http://www.whatismyipaddress.com/',
+        'http://whatismyipaddress.com/',
     )
 
     def parse(self, response):
     	soup = BeautifulSoup(response.body, 'html.parser')
-        ip_container = soup.select('#section_left > div:nth-of-type(2) > a:nth-of-type(1)')[0].encode('UTF-8')
-        ip = BeautifulSoup(ip_container, 'html.parser').get_text()
-        logging.info("IP ADDRESS = %s" % ip)
+
+        ip_container = soup.select('#section_left > div:nth-of-type(2) > a:nth-of-type(1)')
+        if (len(ip_container) > 0):
+            try:
+                ip_container = ip_container[0].encode('UTF-8')
+                ip = BeautifulSoup(ip_container, 'html.parser').get_text()
+                logging.info('IP ADDRESS = %s' % ip)
+            except (RuntimeError, IndexError):
+                logging.info('IP ADDRESS NOT FOUND')
         pass
--- PythonScrapyBasicSetup/spiders/uatester.py
+++ PythonScrapyBasicSetup/spiders/uatester.py
@@ -7,14 +7,14 @@ from bs4 import BeautifulSoup
 
 class UATesterSpider(scrapy.Spider):
     name = 'UAtester'
-    allowed_domains = ['useragentstring.com']
+    allowed_domains = ['user-agent.me']
     start_urls = (
-        'http://www.useragentstring.com/',
+        'http://user-agent.me',
     )
 
     def parse(self, response):
     	soup = BeautifulSoup(response.body, 'html.parser')
-        ua_container = soup.select('#uas_textfeld')[0].encode('UTF-8')
+        ua_container = soup.select('small')[0].encode('UTF-8')
         ua = BeautifulSoup(ua_container, 'html.parser').get_text()
-        logging.info("USER AGENT = %s" % ua)
+        logging.info('USER AGENT = %s' % ua)
         pass

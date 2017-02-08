a2f3bfbcc62b4cf7405ff7892afcc5e78f7b42eb
matejbasic91@gmail.com <matejbasic91@gmail.com>
2017-02-08 22:36:57 +0100

    middleware refactoring

--- PythonScrapyBasicSetup/middlewares.py
+++ PythonScrapyBasicSetup/middlewares.py
@@ -13,31 +13,39 @@ class ProxyMiddleware(object):
 	proxies = []
 
 	def __init__(self, *args, **kwargs):
-		fin = urllib2.urlopen('http://proxylist.hidemyass.com/search-1311281')
-		soup = BeautifulSoup(fin, 'html.parser')
-		# get the table with proxy addresses 
-		data_grid = soup.find('table', id='listable')
-		# iterate through the rows
-		for tr in data_grid.tbody.findAll('tr'):
-			i = 0
-			item = {}
-			for td in tr.findAll('td'):
-				if i == 1: # ip address
-					item['address'] = self.get_ip(td)
-				elif i == 2:
-					item['port'] = td.get_text().strip()
-				elif i == 6:
-					item['protocol'] = td.get_text().lower().strip() 	
-				i += 1
-			# if everything is scraped/defined, save to proxies list
-			if 'ip_address' and 'port' and 'protocol' in item:
-				self.proxies.append(item)
+		self.query_proxies()
 
-		fin.close()
+	def query_proxies(self):
+		request = urllib2.urlopen('http://proxylist.hidemyass.com/search-1311281')
+		if request.getcode() == 200:
+			soup = BeautifulSoup(request, 'html.parser')
+			# get the table with proxy addresses
+			data_grid = soup.find('table', id='listable')
+
+			max_proxies = 100
+			# iterate through the rows
+			for tr in data_grid.tbody.findAll('tr'):
+				i = 0
+				item = {}
+				for td in tr.findAll('td'):
+					if i == 1: # ip address
+						item['address'] = self.get_ip(td)
+					elif i == 2:
+						item['port'] = td.get_text().strip()
+					elif i == 6:
+						item['protocol'] = td.get_text().lower().strip()
+					i += 1
+				# if everything is scraped/defined, save to proxies list
+				if 'ip_address' and 'port' and 'protocol' in item:
+					self.proxies.append(item)
+					max_proxies -= 1
+					if max_proxies < 1:
+						break
+		request.close()
 
 	def get_ip(self, td):
-		# getting ip address is a little bit tricky but this solution 
-		# is simple and fast, for more info check 
+		# getting ip address is a little bit tricky but this solution
+		# is simple and fast, for more info check
 		# https://blueshellgroup.wordpress.com/2013/04/15/creating-a-private-database-of-proxies-part-2/
 		span = td.find('span')
 		styles = str(span.find('style')).split('.')
@@ -57,35 +65,50 @@ class ProxyMiddleware(object):
 		return cls(crawler.settings)
 
 	def process_request(self, request, spider):
-		# don't overwrite with a random one
-		if 'proxy' in request.meta:
-			return
-		
 		item = random.choice(self.proxies)
 		request.meta['proxy'] = item['protocol'] + '://' + item['address'] + ':' + item['port']
-		#print request.meta['proxy']
+		logging.info('Using proxy: %s' % request.meta['proxy'])
 
-	def process_exception(self, request, exception, spider):
+	def remove_failed_proxy(self, request, spider):
 		proxy = request.meta['proxy']
-		print('Removing failed proxy <%s>, %d proxies left' % (proxy, len(self.proxies)))
+		logging.log(logging.DEBUG, 'Removing failed proxy...')
 		try:
 			i = 0;
 			for el in self.proxies:
-				if el['address'] == proxy:
+				if el['address'] in proxy:
 					del self.proxies[i]
-					break
+					proxies_num = len(self.proxies)
+					logging.log(logging.DEBUG, 'Removed failed proxy <%s>, %d proxies left' % (proxy, proxies_num))
+					if proxies_num == 0:
+						self.query_proxies()
+					return True
 				i += 1
-		except ValueError:
-			pass
+		except:
+			logging.log(logging.ERROR, 'Error while removing failed proxy')
+		return False
+
+	def process_exception(self, request, exception, spider):
+		if self.remove_failed_proxy(request, spider):
+			return request
+		return None
+
+
+	def process_response(self, request, response, spider):
+		# really brutal filter
+		if response.status == 200:
+			return response
+		else:
+			return request
 
 class RandomUserAgentMiddleware(object):
 
 	settings = get_project_settings()
 
 	def __init__(self, *args, **kwargs):
+		# print 'RandomUserAgentMiddleware'
 		xmldoc = minidom.parse(urllib2.urlopen('http://techpatterns.com/downloads/firefox/useragentswitcher.xml'))
 		item_list = xmldoc.getElementsByTagName('useragent')
-            
+
 		user_agents = []
 		for s in item_list:
 			user_agents.append(s.attributes['useragent'].value)
@@ -96,4 +119,3 @@ class RandomUserAgentMiddleware(object):
 		ua = random.choice(self.settings.get('USER_AGENT_LIST'))
 		if ua:
 			request.headers.setdefault('User-Agent', ua)
-

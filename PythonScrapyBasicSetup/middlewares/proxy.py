89a7c1ce4087223ae7854418b0924e5ab0c6d9b7
matejbasic91@gmail.com <matejbasic91@gmail.com>
2017-02-19 00:59:43 +0100

    added Tor proxy middleware

--- PythonScrapyBasicSetup/middlewares/proxy.py
+++ PythonScrapyBasicSetup/middlewares/proxy.py
@@ -5,6 +5,42 @@ import random
 from scrapy.utils.project import get_project_settings
 import logging
 
+import getpass
+import stem
+import stem.connection
+from stem import Signal
+from stem.control import Controller
+
+
+class TorProxyMiddleware(object):
+
+	def __init__(self, *args, **kwargs):
+		self.import_settings()
+		self.req_counter = 0
+
+	def change_ip_addres(self):
+		with Controller.from_port(port=self.control_port) as controller:
+			controller.authenticate(self.password)
+			controller.signal(Signal.NEWNYM)
+			controller.close()
+
+	def import_settings(self):
+		settings = get_project_settings()
+		self.control_port 	= settings['CONTROL_PORT']
+		self.password	  	= settings['AUTH_PASSWORD']
+		self.http_proxy	  	= settings['HTTP_PROXY']
+		self.max_req_per_ip = settings['MAX_REQ_PER_IP']
+
+	def process_request(self, request, spider):
+		self.req_counter += 1
+		if self.max_req_per_ip is not None and self.req_counter > self.max_req_per_ip:
+			self.req_counter = 0
+			self.change_ip_addres()
+
+		request.meta['proxy'] = self.http_proxy
+		logging.info('Using proxy: %s' % request.meta['proxy'])
+		return None
+
 class HttpProxyMiddleware(object):
 	proxies = []
 
--- PythonScrapyBasicSetup/settings.py
+++ PythonScrapyBasicSetup/settings.py
@@ -6,7 +6,7 @@ SPIDER_MODULES = ['PythonScrapyBasicSetup.spiders']
 NEWSPIDER_MODULE = 'PythonScrapyBasicSetup.spiders'
 
 # maximum concurrent requests performed by Scrapy (default: 16)
-# CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 32
 # timeout for processing DNS queries in seconds(float) (default: 60)
 DNS_TIMEOUT = 10
 # time(sec) that the downloader will wait before timing out
@@ -29,9 +29,18 @@ RETRY_HTTP_CODES = [500, 502, 503, 504]
 # disable cookies
 COOKIES_ENABLED = False
 
+# TOR SETTINGS
+HTTP_PROXY = 'http://127.0.0.1:8118'
+AUTH_PASSWORD = 'secretPassword'
+CONTROL_PORT = 9051
+# number of HTTTP request before the IP change
+# delete or set to None if you don't want to use it
+MAX_REQ_PER_IP = 1000
+
 # downloader middlewares
 DOWNLOADER_MIDDLEWARES = {
 	'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
 	'PythonScrapyBasicSetup.middlewares.user_agent.RandomUserAgentMiddleware': 400,
-    'PythonScrapyBasicSetup.middlewares.proxy.HttpProxyMiddleware': 410
+    # 'PythonScrapyBasicSetup.middlewares.proxy.HttpProxyMiddleware': 410,
+	'PythonScrapyBasicSetup.middlewares.proxy.TorProxyMiddleware': 410
 }
